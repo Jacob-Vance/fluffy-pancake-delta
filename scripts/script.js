336bfba92a561e5ba1a94cd4dd6e82c433240353
Jacob-Vance <54328477+Jacob-Vance@users.noreply.github.com>
2024-02-08 11:38:57 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
+$(document).ready(function() {
+    console.log( "ready!" );
+    $("#goButton").click(function(){
+                  
+      let $fname = $("#fname").val();
+      $("#userMsg").text("Greetings " + $fname +". You are super cool and fun.");
+  
+    });
 
+});
