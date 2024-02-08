1ab8b67d50c085f2512b7f20491fb0cc6fcb1ad8
Jacob-Vance <54328477+Jacob-Vance@users.noreply.github.com>
2024-02-08 12:16:23 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
-    console.log( "ready!" );
-    $("#goButton").click(function(){
-                  
-      let $fname = $("#fname").val();
-      $("#userMsg").text("Greetings " + $fname +". You are super cool and fun.");
-  
-    });
+  console.log( "button clicked" );
+  $("#goButton").click(function(){
+                
+    let username = $("#username").val();
+    $("#userMsg").text("Greetings " + username +". You are super cool and fun.");
+//     let spiritaniaml = generatespiritanaimal(username); 
+
+  });
 
 });
+
+// function generatespiritanaimal(username) {
+//   let spiritaniaml = ''; 
+
+//   spiritaniaml = " <h3> " + namestart(username) + " </h3> ";
+  
+// }
+
+// function namestart(name) {
+//   return "the first letter of your name is " + name[0]
+
+// }
