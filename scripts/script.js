687fd352f5eaf145af4fd9bc1406c256a87a79ef
Jacob-Vance <54328477+Jacob-Vance@users.noreply.github.com>
2024-02-08 11:31:01 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  
+  console.log("Top: " + $("img").position().top + "Left: " + $("img").position().left);
+  
+  // $("img").draggable({
+  //   stop: function () {
+  //     console.log("stopping???");
+
+  //     let dogeLeft = $("img").position().left;
+
+  //     if (dogeLeft > 100, dogeLeft <= 1000) {
+  //       console.log(dogeLeft);
+
+  //       $("p").text("Wow, you got " + dogeLeft + " points. Good for you.");
+
+  //     }
+  //     if (dogeLeft > 1000) {
+  //       console.log(dogeLeft);
+
+  //       $("p").text("Wow, you got " + dogeLeft + " points. You win.");
+
+  //     }
+  //   },
+  // });
+
+ 
+  $( "#draggable3" ).draggable(
+    { 
+      axis: "x" 
+    });
+});
