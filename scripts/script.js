a4455686f1e8cad703d2f67138aae394f9ef63d9
Jacob-Vance <54328477+Jacob-Vance@users.noreply.github.com>
2024-02-13 15:55:46 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
 $(function() {
   console.log("document is ready!");
 
-  var username, firstletter, msg1, msg2, spiritaniaml = ""; 
+  var username, firstletter, msg1, msg2, spiritanimal = ""; 
 
   $('#namebutton').click(function() {
       username = $('#username').val();
 
       firstletter = getirstletter(username);
-      spiritaniaml = getspiritaniaml(username);
+      spiritanimal = getspiritaniaml(username);
       msg1 = "The first letter of your name is ";
-      msg2 = ", which means your spirit aniaml is a(n) ";
-      $('#nameoutput').text(msg1 + firstletter + msg2 + spiritaniaml +"!"); 
+      msg2 = ", which means your spirit animal is a(n) ";
+      $('#nameoutput').text(msg1 + firstletter + msg2 + spiritanimal +"!"); 
 
       });
 
@@ -20,89 +20,89 @@ $(function() {
   }
 
   function getspiritaniaml(nameval){
-      var  thespiritaniaml = "";
+      var  thespiritanimal = "";
       if (nameval[0] === "a" || nameval[0] === "A"){
-          thespiritaniaml = "Antelope";
+          thespiritanimal = "Antelope";
       }
       else if (nameval[0] === "b" || nameval[0] === "B"){
-          thespiritaniaml = "Bear";
+          thespiritanimal = "Bear";
       }
       else if (nameval[0] === "c" || nameval[0] === "C"){ 
-          thespiritaniaml = "Cat";
+          thespiritanimal = "Cat";
       }
       else if (nameval[0] === "d" || nameval[0] === "D"){
-          thespiritaniaml = "Dog";
+          thespiritanimal = "Dog";
       }
       else if (nameval[0] === "e" || nameval[0] === "E"){
-          thespiritaniaml = "Elephant";
+          thespiritanimal = "Elephant";
       }
       else if (nameval[0] === "f" || nameval[0] === "F"){
-          thespiritaniaml = "Fox";
+          thespiritanimal = "Fox";
       }
       else if (nameval[0] === "g" || nameval[0] === "G"){
-          thespiritaniaml = "Giraffe";
+          thespiritanimal = "Giraffe";
       }
       else if (nameval[0] === "h" || nameval[0] === "H"){
-          thespiritaniaml = "Horse";
+          thespiritanimal = "Horse";
       }
       else if (nameval[0] === "i" || nameval[0] === "I"){
-          thespiritaniaml = "Iguana";
+          thespiritanimal = "Iguana";
       }
       else if (nameval[0] === "j" || nameval[0] === "J"){
-          thespiritaniaml = "Jaguar";
+          thespiritanimal = "Jaguar";
       }
       else if (nameval[0] === "k" || nameval[0] === "K"){
-          thespiritaniaml = "Kangaroo";
+          thespiritanimal = "Kangaroo";
       }
       else if (nameval[0] === "l" || nameval[0] === "L"){
-          thespiritaniaml = "Lion";
+          thespiritanimal = "Lion";
       }
       else if (nameval[0] === "m" || nameval[0] === "M"){
-          thespiritaniaml = "Monkey";
+          thespiritanimal = "Monkey";
       }
       else if (nameval[0] === "n" || nameval[0] === "N"){
-          thespiritaniaml = "Narwhal";
+          thespiritanimal = "Narwhal";
       }
       else if (nameval[0] === "o" || nameval[0] === "O"){
-          thespiritaniaml = "Owl";
+          thespiritanimal = "Owl";
       }
       else if (nameval[0] === "p" || nameval[0] === "P"){
-          thespiritaniaml = "Penguin";
+          thespiritanimal = "Penguin";
       }
       else if (nameval[0] === "q" || nameval[0] === "Q"){
-          thespiritaniaml = "Quail";
+          thespiritanimal = "Quail";
       }
       else if (nameval[0] === "r" || nameval[0] === "R"){
-          thespiritaniaml = "Rabbit";
+          thespiritanimal = "Rabbit";
       }
       else if (nameval[0] === "s" || nameval[0] === "S"){
-          thespiritaniaml = "Snake";
+          thespiritanimal = "Snake";
       }
       else if (nameval[0] === "t" || nameval[0] === "T"){
-          thespiritaniaml = "Tiger";
+          thespiritanimal = "Tiger";
       }
       else if (nameval[0] === "u" || nameval[0] === "U"){
-          thespiritaniaml = "Uakari";
+          thespiritanimal = "Uakari";
       }
       else if (nameval[0] === "v" || nameval[0] === "V"){
-          thespiritaniaml = "Vulture";
+          thespiritanimal = "Vulture";
       }
       else if (nameval[0] === "w" || nameval[0] === "W"){
-          thespiritaniaml = "Whale";
+          thespiritanimal = "Whale";
       }
       else if (nameval[0] === "x" || nameval[0] === "X"){
-          thespiritaniaml = "Xerus";
+          thespiritanimal = "Xerus";
       }
       else if (nameval[0] === "y" || nameval[0] === "Y"){
-          thespiritaniaml = "Yak";
+          thespiritanimal = "Yak";
       }
       else if (nameval[0] === "z" || nameval[0] === "Z"){
-          thespiritaniaml = "Zebra";
+          thespiritanimal = "Zebra";
       }
       else {
-          thespiritaniaml = "Error (Please enter a different name)";
+          thespiritanimal = "Error (Please enter a different name)";
       }
-     return thespiritaniaml; 
+     return thespiritanimal; 
   }
 
 });
